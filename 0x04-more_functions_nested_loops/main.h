919e4255aed15e866d098bcd923c5c2ed659b286
yo-hov <macdayoola@gmail.com>
2023-06-23 05:12:40 -0700

    Hi

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#indef MAIN_H
+#define MAIN H
+
+int _putchar(char c),
+int _isupper(int c);
+int _isdigit(int c),
+int mul (int a, int b),
+void print _numbers/(void),
+void print_most numbers(void),
+void more_numbers/void),
+void print _line(int n);
+void print_diagonal(int n),
+void print_ square(int size),
+void print_tangle(int size),
+
+#endif
